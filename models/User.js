ca03aff811c40c2707f725c8b876840959be404a
anmolkr2003 <anmolraj112233@gmail.com>
2025-10-29 00:04:50 +0530

    sadf

--- models/User.js
+++ models/User.js
@@ -8,6 +8,6 @@ const userSchema = new mongoose.Schema({
   isVerified: { type: Boolean, default: false },
   verificationToken: String
 });
-userSchema.pre('save', async function(next){ if(!this.isModified('password')) return next(); const salt= await bcrypt.genSalt(10); this.password = await bcrypt.hash(this.password, salt); next(); });
+// userSchema.pre('save', async function(next){ if(!this.isModified('password')) return next(); const salt= await bcrypt.genSalt(10); this.password = await bcrypt.hash(this.password, salt); next(); });
 userSchema.methods.comparePassword = function(candidate){ return bcrypt.compare(candidate, this.password); }
 module.exports = mongoose.model('User', userSchema);
